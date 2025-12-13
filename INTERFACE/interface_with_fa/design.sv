1468bdacbea067eee3635b66b9fe44839fc6a88f
Srujith <23211a04p9@bvrit.ac.in>
2025-12-13 18:34:11 +0530

    .

--- INTERFACE/interface_with_fa/design.sv
+++ INTERFACE/interface_with_fa/design.sv
@@ -1,7 +1,17 @@
+//==========================================================//
+//-------------INTERFACE------------------------------------//
+//==========================================================//
+
 interface adder_intf ;
   logic a,b,cin,s,c;
+  modport dut(input a,b,cin,output s,c);
+  modport tb (input s,c, output a,b,cin);
+  
 endinterface
 
+//================================================//
+//--===========DESIGN============================//
+//==============================================//
 module fa(adder_intf inf);
   assign {inf.s,inf.c}=inf.a+inf.b+inf.cin;
 endmodule
